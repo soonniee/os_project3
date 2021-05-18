bc0ffbac3a787def04afa1d4299ccf1b954ac755
soonniee <ckstns1231@gmail.com>
2021-05-18 23:21:14 +0900

    주석처리

--- main.cc
+++ main.cc
@@ -11,9 +11,21 @@ int *lruFrame;
 int *lfuFrame;
 int *lfuCount;
 vector<int> wsPage;
+// pageNum : process가 갖는 page 수
+// pageFrameNum : 할당 page frame 수
+// windowSize : window size
+// referLen : page reference string 길이
+// referString : page reference string 저장된 배열
+// minFrame: MIN 알고리즘을 위한 page frame 배열
+// fifoFrame : FIFO 알고리즘을 위한 page frame 배열
+// lruFrame : LRU 알고리즘을 위한 page frame 배열
+// lfuFrame : LFU 알고리즘을 위한 page frame 배열
+// lfuCount : Page reference count 저장될 배열
+// wsPage : WS 알고리즘 Memory State 보여줄 벡터
 void hanleInput(){
     FILE *fp;
     fp = fopen("input.txt","r");
+    
     fscanf(fp,"%d %d %d %d",&pageNum,&pageFrameNum,&windowSize,&referLen);
     referString = (int*)malloc(sizeof(int)*referLen);
     minFrame = (int*)malloc(sizeof(int)*pageFrameNum);
@@ -37,12 +49,13 @@ void printMIN(int index, bool fault){
 }
 
 void handleMIN(int pageFaultNum, int index){
+    // page fault 발생하였지만 page frame 내 empty 존재하는 경우.
     if(pageFaultNum <= pageFrameNum){
         minFrame[pageFaultNum - 1] = referString[index];
         printMIN(index,true);
     }else{
+        // page fault 발생하였고 page frame 내 empty 존재하지 않는 경우.
         int replacedIndex = -1;
-        
         int max = -1;
         int tieBreak = pageNum;
         int tieBreakIndex = -1;
@@ -61,6 +74,8 @@ void handleMIN(int pageFaultNum, int index){
                 }
             }
             if(!refered){
+                // 미래에 refered 되지 않는 경우 따로 처리.
+                // 두 개 이상의 page 존재 시 tie break rule 적용.
                 if(tieBreak > minFrame[i]){
                     tieBreak = minFrame[i];
                     tieBreakIndex = i;
@@ -75,6 +90,7 @@ void handleMIN(int pageFaultNum, int index){
     }
 }
 void MIN(){
+    //minFrame 배열 -1로 초기화. -> -1은 empty 함을 뜻함.
     for(int i=0;i<pageFrameNum;i++){
         minFrame[i] = -1;
     }
@@ -82,17 +98,20 @@ void MIN(){
     for(int i=0;i<referLen;i++){
         bool pageFault = true;
         for(int j=0;j<pageFrameNum;j++){
+            //Page fault 아닐 경우 -> printMin 함수 호출.
             if(referString[i] == minFrame[j]){
                 pageFault = false;
-                printMIN(i,false);
+                printMIN(i,false); // Memory State 출력 함수.
                 break;
             }
         }
+        //Page fault 발생할 경우 -> handleMIN 함수 호출.
         if(pageFault){
             pageFaultNum++;
-            handleMIN(pageFaultNum, i);
+            handleMIN(pageFaultNum, i); // MIN 알고리즘 수행 함수.
         }
     }
+    // MIN 알고리즘 수행 후 총 Page fault 갯수 출력.
     cout << "Total Page Fault Count : " << pageFaultNum << '\n' << '\n';
 } 
 void printFIFO(int index, bool fault){
@@ -103,15 +122,14 @@ void printFIFO(int index, bool fault){
         if(fifoFrame[i]!=-1) cout << fifoFrame[i] << ' ' ;
     }
     cout << '\n';
-   
-
 }
 void handleFIFO(int pageFaultNum, int index){
+    // page fault 발생하였지만 page frame 내 empty 존재하는 경우.
     if(pageFaultNum <= pageFrameNum){
         fifoFrame[pageFaultNum - 1] = referString[index];
-        
         printFIFO(index,true);
     }else{
+        // page fault 발생하였고 page frame 내 empty 존재하지 않는 경우.
         int replacedIndex = -1;
         replacedIndex = (pageFaultNum-1) % pageFrameNum;
         
@@ -121,6 +139,7 @@ void handleFIFO(int pageFaultNum, int index){
     }
 }
 void FIFO(){
+    //fifoFrame 배열 -1로 초기화. -> -1은 empty 함을 뜻함.
     for(int i=0;i<pageFrameNum;i++){
         fifoFrame[i] = -1;
     }
@@ -128,15 +147,17 @@ void FIFO(){
     for(int i=0;i<referLen;i++){
         bool pageFault = true;
         for(int j=0;j<pageFrameNum;j++){
+            //Page fault 아닐 경우 -> printFIFO 함수 호출.
             if(referString[i] == fifoFrame[j]){
                 pageFault = false;
-                printFIFO(i,false);
+                printFIFO(i,false); // Memory State 출력 함수.
                 break;
             }
         }
+        //Page fault 발생할 경우 -> handleFIFO 함수 호출.
         if(pageFault){
             pageFaultNum++;
-            handleFIFO(pageFaultNum, i);
+            handleFIFO(pageFaultNum, i); // FIFO 알고리즘 수행 함수.
         }
     }
     cout << "Total Page Fault Count : " << pageFaultNum << '\n' << '\n';
@@ -154,18 +175,17 @@ void printLRU(int index, bool fault){
 
 }
 void handleLRU(int pageFaultNum, int index){
+    // page fault 발생하였지만 page frame 내 empty 존재하는 경우.
     if(pageFaultNum <= pageFrameNum){
         lruFrame[pageFaultNum - 1] = referString[index];
         // q.push(index);
         printLRU(index,true);
     }else{
-        
+        // page fault 발생하였고 page frame 내 empty 존재하지 않는 경우.
         int replacedIndex = -1;
-        
         int min = referLen;
         int tieBreak = pageNum;
         int tieBreakIndex = -1;
-        // printf("%d\n",index);
         for(int i=0;i<pageFrameNum;i++){
             bool refered = false;
             for(int j=index-1;j>=0;j--){
@@ -180,6 +200,8 @@ void handleLRU(int pageFaultNum, int index){
                 }
             }
             if(!refered){
+                // 이전에 refered 되지 않은 경우 따로 처리.
+                // 두 개 이상의 page 존재 시 tie break rule 적용.
                 if(tieBreak > minFrame[i]){
                     tieBreak = minFrame[i];
                     tieBreakIndex = i;
@@ -195,6 +217,7 @@ void handleLRU(int pageFaultNum, int index){
     }
 }
 void LRU(){
+    //lruFrame 배열 -1로 초기화. -> -1은 empty 함을 뜻함.
     for(int i=0;i<pageFrameNum;i++){
         lruFrame[i] = -1;
     }
@@ -202,15 +225,17 @@ void LRU(){
     for(int i=0;i<referLen;i++){
         bool pageFault = true;
         for(int j=0;j<pageFrameNum;j++){
+            //Page fault 아닐 경우 -> printLRU 함수 호출.
             if(referString[i] == lruFrame[j]){
                 pageFault = false;
                 printLRU(i,false);
                 break;
             }
         }
+        //Page fault 발생할 경우 -> handleLRU 함수 호출.
         if(pageFault){
             pageFaultNum++;
-            handleLRU(pageFaultNum, i);
+            handleLRU(pageFaultNum, i); // LRU 알고리즘 수행 함수.
         }
     }
     cout << "Total Page Fault Count : " << pageFaultNum << '\n' << '\n';
@@ -226,27 +251,25 @@ void printLFU(int index, bool fault){
 
 }
 void handleLFU(int pageFaultNum, int index){
+    // page fault 발생하였지만 page frame 내 empty 존재하는 경우.
     if(pageFaultNum <= pageFrameNum){
         lfuFrame[pageFaultNum - 1] = referString[index];
         printLFU(index,true);
     }else{
-        
+        // page fault 발생하였고 page frame 내 empty 존재하지 않는 경우.
         int replacedIndex = -1;
         int min = referLen;
         int max = referLen;
         int tieBreak = pageNum;
         int tieBreakIndex = -1;
-        
         for(int i=0;i<pageFrameNum;i++){
             int frequency = lfuCount[lfuFrame[i]];
-            
             if(max < frequency) continue;
             else if(max==frequency){
-                
+                // page reference count 수 가장 적은 page가 여러 개 존재 -> Tie-breaking rule 적용
                 bool refered = false;
                 for(int j=index-1;j>=0;j--){
                     if(lfuFrame[i] == referString[j]){
-                        
                         if(min > j){
                             replacedIndex = i;
                             min = j;
@@ -260,7 +283,6 @@ void handleLFU(int pageFaultNum, int index){
                         tieBreak = lfuFrame[i];
                         tieBreakIndex = i;
                     }
-                    
                 }
             }
             else{
@@ -276,18 +298,18 @@ void handleLFU(int pageFaultNum, int index){
                     }
                 }
             }
-            
         }
         if(tieBreakIndex == -1) lfuFrame[replacedIndex] = referString[index];
         else lfuFrame[tieBreakIndex] = referString[index];
         printLFU(index,true);
-
     }
 }
 void LFU(){
+    //lfuFrame 배열 -1로 초기화. -> -1은 empty 함을 뜻함.
     for(int i=0;i<pageFrameNum;i++){
         lfuFrame[i] = -1;
     }
+     //lfuCount 배열 0로 초기화.
     for(int i=0;i<pageNum;i++){
         lfuCount[i] = 0;
     }
@@ -296,15 +318,17 @@ void LFU(){
         bool pageFault = true;
         lfuCount[referString[i]] += 1;
         for(int j=0;j<pageFrameNum;j++){
+            //Page fault 아닐 경우 -> printLFU 함수 호출.
             if(referString[i] == lfuFrame[j]){
                 pageFault = false;
                 printLFU(i,false);
                 break;
             }
         }
+        //Page fault 발생할 경우 -> handleLFU 함수 호출.
         if(pageFault){
             pageFaultNum++;
-            handleLFU(pageFaultNum, i);
+            handleLFU(pageFaultNum, i); // LFU 알고리즘 수행 함수.
         }
     }
     cout << "Total Page Fault Count : " << pageFaultNum << '\n' << '\n';
@@ -320,9 +344,9 @@ void printWS(int index, bool fault){
 }
 void handleWS(int index, bool fault){
     int refered = referString[index];
-    vector<int> popArr;
-    
+    vector<int> popArr; //replace될 page 저장할 벡터.
     if(index < windowSize){
+        // wsPage : WS 알고리즘 Memory State 저장할 벡터
         for(int i=0;i<wsPage.size();i++){
              bool pop = true;
             for(int j=index-1;j>=0;j--){
@@ -331,45 +355,40 @@ void handleWS(int index, bool fault){
                     break;
                 }
             }
-            if(pop) popArr.push_back(i);
+            if(pop) popArr.push_back(i); //replace될 page를 popArr에 push.
         }
         
     }else{
-        
         for(int i=0;i<wsPage.size();i++){
             bool pop = true;
-            
             for(int j=index-1;j>=index-windowSize;j--){
                 if(wsPage[i] == referString[j]|| refered == wsPage[i]){
-                    
                     pop = false;
                     break;
                 }
             }
-            if(pop) popArr.push_back(i);
+            if(pop) popArr.push_back(i); //replace될 page push
         }
     }
-   
     for(int i=0;i<popArr.size();i++){
-        
-        wsPage.erase(wsPage.begin() + popArr[i]);
+        wsPage.erase(wsPage.begin() + popArr[i]); //replace될 page를 wsPage에서 삭제.
     }
-    if(fault) wsPage.push_back(refered);
+    if(fault) wsPage.push_back(refered); //page fault 발생 -> 해당 page를 wsPage에 push.
     printWS(index,fault);
-    
 }
 void WS(){
-    
     int pageFaultNum = 0;
     for(int i=0;i<referLen;i++){
         bool pageFault = true;
         for(int j=0;j<wsPage.size();j++){
+            //Page fault 아닐 경우 -> handleWS 함수 호출.
             if(referString[i] == wsPage[j]){
                 pageFault = false;
                 handleWS(i,false);
                 break;
             }
         }
+        //Page fault 경우 -> handleWS 함수 호출.
         if(pageFault){
             pageFaultNum++;
             handleWS(i,true);
@@ -378,9 +397,9 @@ void WS(){
     cout << "Total Page Fault Count : " << pageFaultNum << '\n' << '\n';
 }
 void printTop(){
-    cout << "+------+-------+--------+--------\n";
-    cout << "| Time | Ref.S | Page.F | Memory\n";
-    cout << "+------+-------+--------+--------\n";
+    cout << "+------+-------+--------+-------------\n";
+    cout << "| Time | Ref.S | Page.F | Memory State\n";
+    cout << "+------+-------+--------+-------------\n";
 } 
 int main(){
     hanleInput();
